c829dc70814e3e7389114af9ed8a08f46cc4308a
root <elesrubsoap_4822@hotmail.com>
2018-12-21 21:22:23 +0700

    add testQuerySelect.php

--- actuallyUse/testQuerySelect.php
+++ actuallyUse/testQuerySelect.php
@@ -1,13 +1,13 @@
 <?php
 require 'connect.php';
 
-$sql = "SELECT * FROM complaint";
+$sql = "SELECT * FROM category";
 $result = mysqli_query($conn,$sql);
 
-$selectObj = '<select name=\"cds\" onchange=\"showCD(this.value)\">'.'<option value="">Select a CD:</option>';
+$selectObj = '<select name=\"cds\" onchange=\"showCD(this.value)\">' . '<option value=\"\">Select a CD:</option>';
 
 while($row = mysqli_fetch_array($result)) {
-    $selectObj = $selectObj .'<option value=\".$row['idcat']'. '\">' . '$row['catname']' . '</option>"';
+    $selectObj = $selectObj .'<option value=\"' .$row['idcat']. '\">' . $row['catname'] . '</option>"';
 }
 
 $selectObj = $selectObj . '</select>';
