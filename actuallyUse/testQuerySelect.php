7db7e85a501d814f04b2fc8fc92cbf6c2329b06e
root <elesrubsoap_4822@hotmail.com>
2018-12-21 20:00:11 +0700

    solve

--- actuallyUse/testQuerySelect.php
+++ actuallyUse/testQuerySelect.php
@@ -1,14 +1,14 @@
 <?php
 require 'connect.php';
 
-$sql = "SELECT * FROM complaint;
+$sql = "SELECT * FROM complaint";
 $result = mysqli_query($conn,$sql);
 
 $selectObj = "<select name=\"cds\" onchange=\"showCD(this.value)\">"
-.<option value="">Select a CD:</option>;
+."<option value="">Select a CD:</option>";
 
 while($row = mysqli_fetch_array($result)) {
-    $selectObj = ."<option value=\".$row['idcat'].\">.$row['catname'].</option>"
+    $selectObj = ."<option value=\".$row['idcat'].\">.$row['catname'].</option>";
 }
 
 $selectObj = ."</select>";
