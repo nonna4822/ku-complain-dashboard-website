43a9462fffc0105460f0ee4125627d8d1f98f05a
root <elesrubsoap_4822@hotmail.com>
2018-12-21 19:54:56 +0700

    add testSelectQuery

---
+++ actuallyUse/loginSystem/getlogin.php
@@ -0,0 +1,36 @@
+<?php
+
+//session
+session_start();
+
+require '../connect.php';
+
+// Set php variables
+$username = mysqli_real_escape_string($conn,$_GET['username']);
+$password =  mysqli_real_escape_string($conn,$_GET['password']);
+
+// queryWHERE cardno = '$cardno' AND tel = '$tel'"
+$sql = "SELECT * FROM student WHERE username = '$username'";
+
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result) == 1){
+  $row = mysqli_fetch_assoc($result);
+  if($row["password"] == $password){
+    //set session var
+    $_SESSION['username'] = $row["username"];
+    $_SESSION['name'] = $row["firstname"] . " " . $row["lastname"];
+
+    //go to homepage. ( logged )
+    header("Location: ../home.php"); //go to student view
+    exit;
+  }else {
+    echo "username or password is wrong";
+  }
+}else {
+  echo "not found anyone ";
+}
+
+mysqli_close($conn);
+
+?>
---
+++ actuallyUse/testQuerySelect.php
@@ -0,0 +1,20 @@
+<?php
+require 'connect.php';
+
+$sql = "SELECT * FROM complaint;
+$result = mysqli_query($conn,$sql);
+
+$selectObj = "<select name=\"cds\" onchange=\"showCD(this.value)\">"
+.<option value="">Select a CD:</option>;
+
+while($row = mysqli_fetch_array($result)) {
+    $selectObj = ."<option value=\".$row['idcat'].\">.$row['catname'].</option>"
+}
+
+$selectObj = ."</select>";
+
+echo $selectObj;
+
+mysqli_close($conn);
+echo "narawit";
+ ?>
