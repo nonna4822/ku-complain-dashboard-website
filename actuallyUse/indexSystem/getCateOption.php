e56e9d22c23ac3f250cfa588fd3e09eae21c8832
root <elesrubsoap_4822@hotmail.com>
2018-12-22 04:12:20 +0700

    6666

--- actuallyUse/indexSystem/getCateOption.php
+++ actuallyUse/indexSystem/getCateOption.php
@@ -6,7 +6,7 @@ $result = mysqli_query($conn,$sql);
 $selectObj = '<select onload="showComplaint(this.value)" onchange="showComplaint(this.value)" class="selectpicker">';
 
 while($row = mysqli_fetch_array($result)) {
-    $selectObj = $selectObj.'<option value="'.$row['catname'].'">'.$row['catname'].'</option>';
+    $selectObj = $selectObj.'<option value="'.$row['idcat'].'">'.$row['catname'].'</option>';
 }
 
 $selectObj = $selectObj . '</select>';
