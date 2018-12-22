be96e3d8ddecbbbfb8f42cbb673d8b6db223c1fc
root <elesrubsoap_4822@hotmail.com>
2018-12-22 14:13:12 +0700

    go

--- actuallyUse/loginSystem/getlogin.php
+++ actuallyUse/loginSystem/getlogin.php
@@ -10,19 +10,19 @@ $username = mysqli_real_escape_string($conn,$_GET['username']);
 $password =  mysqli_real_escape_string($conn,$_GET['password']);
 
 // queryWHERE cardno = '$cardno' AND tel = '$tel'"
-$sql = "SELECT * FROM student WHERE username = '$username'";
+$sql = "SELECT * FROM student WHERE sid = '$username'";
 
 $result = mysqli_query($conn, $sql);
 
 if(mysqli_num_rows($result) == 1){
   $row = mysqli_fetch_assoc($result);
-  if($row["password"] == $password){
+  if($row["spassword"] == $password){
     //set session var
-    $_SESSION['username'] = $row["username"];
-    $_SESSION['name'] = $row["firstname"] . " " . $row["lastname"];
+    $_SESSION['username'] = $row["sid"];
+    $_SESSION['name'] = $row["stuname"] . " " . $row["stuLastname"];
 
     //go to homepage. ( logged )
-    header("Location: ../home.php"); //go to student view
+    header("Location: ../home_stu.php"); //go to student view
     exit;
   }else {
     echo "username or password is wrong";
